2ecb0c9fea1d7ef16874ffe64244abe0225cdbbe
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2023-11-11 18:51:09 -0500

    parametrized private key

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import dotenv from 'dotenv'
 import fs from 'fs'
-import http from 'http'
+// import http from 'http'
 import { Octokit, App } from 'octokit'
 import { createNodeMiddleware } from '@octokit/webhooks'
+import { Config } from "sst/node/config";
 
 // Load environment variables from .env file
 dotenv.config()
 
 // Set configured values
 const appId = process.env.APP_ID
-const privateKeyPath = process.env.PRIVATE_KEY_PATH
-const privateKey = fs.readFileSync(privateKeyPath, 'utf8')
+// const privateKeyPath = process.env.PRIVATE_KEY_PATH
+// const privateKey = fs.readFileSync(privateKeyPath, 'utf8')
+const privateKey = Buffer.from(Config.GITHUB_PRIVATE_KEY, 'utf-8')
 const secret = process.env.WEBHOOK_SECRET
 const enterpriseHostname = process.env.ENTERPRISE_HOSTNAME
 const messageForNewPRs = fs.readFileSync('./message.md', 'utf8')
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,9 +1,16 @@
-import { StackContext, Api, EventBus } from "sst/constructs";
+import { StackContext, Api, Function, Config } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
+  const GITHUB_PRIVATE_KEY = new Config.Secret(stack, "GITHUB_PRIVATE_KEY")
+
+  const handlerFunc = new Function(stack, "handlerFunc", {
+    handler: "packages/functions/src/lambda.handler",
+    bind: [GITHUB_PRIVATE_KEY]
+  })
+
   const api = new Api(stack, "api", {
     routes: {
-      "POST /": "packages/functions/src/lambda.handler",
+      "POST /": handlerFunc,
     },
   });
 
