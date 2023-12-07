b2d2b01fbc7d9db14eae62cd5ede71da64cd1c03
Scott Bergeron <scott.bergeron@instacart.com>
2023-12-06 21:51:00 -0500

    modified example to perform status checks

--- app.js
+++ app.js
@@ -35,21 +35,131 @@ const { data } = await app.octokit.request('/app')
 // Read more about custom logging: https://github.com/octokit/core.js#logging
 app.octokit.log.debug(`Authenticated as '${data.name}'`)
 
+async function pullRequestHasRelevantChanges(octokit, repository, pull_request) {
+  // Get the files altered in the pull request
+  const files = await octokit.rest.pulls.listFiles({
+    owner: repository.owner.login,
+    repo: repository.name,
+    pull_number: pull_request.number,
+  })
+
+  // Show file names (debug)
+  files.data.forEach(file => console.log(file.filename))
+
+  // Check if the pull request contains a relevant file
+  const hasRelevantChanges = files.data.some(file => file.filename.toLowerCase() === 'readme.md')
+  console.log("Contains " + (hasRelevantChanges ? "" : "no ") + "relevant files")
+  return hasRelevantChanges
+}
+
+async function pullRequestAltered(octokit, repository, pull_request) {
+  const hasRelevantChanges = await pullRequestHasRelevantChanges(octokit, repository, pull_request)
+
+  if (hasRelevantChanges) {
+    try {
+      await octokit.rest.checks.create({
+        owner: repository.owner.login,
+        repo: repository.name,
+        name: 'Example Check Suite',
+        head_sha: pull_request.head.sha,
+        status: 'in_progress',
+      })
+    } catch (error) {
+      if (error.response) {
+        console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
+      } else {
+        console.error(error)
+      }
+    }
+
+    // Wait 10 seconds, simulate logic
+    await new Promise(r => setTimeout(r, 10000))
+
+    try {
+      await octokit.rest.checks.create({
+        owner: repository.owner.login,
+        repo: repository.name,
+        name: 'Example Check Suite',
+        head_sha: pull_request.head.sha,
+        status: 'completed',
+        conclusion: 'failure',
+      })
+    } catch (error) {
+      if (error.response) {
+        console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
+      } else {
+        console.error(error)
+      }
+    }
+  }
+}
+
 // Subscribe to the "pull_request.opened" webhook event
 app.webhooks.on('pull_request.opened', async ({ octokit, payload }) => {
-  console.log(`Received a pull request event for #${payload.pull_request.number}`)
-  try {
-    await octokit.rest.issues.createComment({
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      issue_number: payload.pull_request.number,
-      body: messageForNewPRs
-    })
-  } catch (error) {
-    if (error.response) {
-      console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
-    } else {
-      console.error(error)
+  console.log(`Received a pull request opened event for #${payload.pull_request.number}`)
+
+  pullRequestAltered(octokit, payload.repository, payload.pull_request)
+})
+
+// Subscribe to the "pull_request.synchronize" webhook event
+app.webhooks.on('pull_request.synchronize', async ({ octokit, payload }) => {
+  console.log(`Received a pull request synchronize event for #${payload.pull_request.number}`)
+
+  pullRequestAltered(octokit, payload.repository, payload.pull_request)
+})
+
+// Subscribe to the "check_suite.rerequested" webhook event
+app.webhooks.on('check_suite.rerequested', async ({ octokit, payload }) => {
+  console.log(`Received a check suite rerequested event for #${payload.check_suite.id}`)
+
+  payload.check_suite.pull_requests.forEach(pull_request => {
+    pullRequestAltered(octokit, payload.repository, pull_request)
+  })
+})
+
+// Subscribe to the "check_run.rerequested" webhook event
+app.webhooks.on('check_run.rerequested', async ({ octokit, payload }) => {
+  console.log(`Received a check run rerequested event for #${payload.check_run.id}`)
+
+  payload.check_run.pull_requests.forEach(pull_request => {
+    pullRequestAltered(octokit, payload.repository, pull_request)
+  })
+})
+
+// Subscribe to the "issue_comment.created" webhook event
+app.webhooks.on('issue_comment.created', async ({ octokit, payload }) => {
+  console.log(`Received an issue comment created event for #${payload.issue.number}`)
+
+  // Check if the comment is on a pull request
+  if (payload.issue.pull_request) {
+    // Check if the comment contains the magic phrase
+    if (payload.comment.body.toLowerCase().includes('please unblock')) {
+      // Post a comment on the pull request
+      try {
+        const { data: pullRequest } = await octokit.rest.pulls.get({
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          pull_number: payload.issue.number,
+        });
+
+        // Extract the head SHA from the pull request
+        const headSha = pullRequest.head.sha;
+
+        await octokit.rest.checks.create({
+          owner: payload.repository.owner.login,
+          repo: payload.repository.name,
+          name: 'Example Check Suite',
+          head_sha: headSha,
+          status: 'completed',
+          conclusion: 'success',
+        })
+      } catch (error) {
+        if (error.response) {
+          console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
+        } else {
+          console.error(error)
+        }
+      }
     }
   }
 })
